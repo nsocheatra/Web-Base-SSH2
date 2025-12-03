8ba21583d03a5d60ae54e9ad0f0f01589118edec
nsocheatra <nongsocheatra27@gmail.com>
2025-12-03 17:14:21 +0700

    remove something

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,10 @@
-import { SessionProvider } from "next-auth/react";
 import { SidebarProvider } from "@/context/SidebarContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
       <SidebarProvider>
         <Component {...pageProps} />
       </SidebarProvider>
-    </SessionProvider>
   );
 }
